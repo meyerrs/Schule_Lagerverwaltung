4d400bb620bcfe559a748ea7117697a138f88719
meyerrs <sb.meyer.yannis@gmail.com>
2025-11-17 12:34:54 +0100

    finished inventory entity

--- backend/src/Entity/Inventory.php
+++ backend/src/Entity/Inventory.php
@@ -7,6 +7,67 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Table(name : 'Inventory')]
 class Inventory
 {
+    #[ORM\Column(type: Types:: INTEGER)]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
     private int $id;
 
+    #[ORM\Column(type: Types:: STRING)]
+    private string $name;
+
+    #[ORM\Column(type: Types:: STRING)]
+    private string $abteilung;  
+
+    #[ORM\Column(type: Types:: STRING)]
+    private string $gruppe;
+
+    #[ORM\Column(type: Types:: STRING)]
+    private string $fach;
+
+    #[ORM\Column(type: Types:: STRING)]
+    private string $ort;
+
+
+    public function getID(){
+        return $this->id;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setName(string $name){
+        return $this->name = $name;
+    }
+
+    public function getAbteilung(){
+        return $this->abteilung;
+    }
+
+    public function setAbteilung(string $abteilung){
+        return $this->abteilung = $abteilung;
+    }
+
+    public function getGruppe(){
+        return $this->gruppe;
+    }
+
+    public function setGruppe(string $gruppe){
+        return $this->gruppe = $gruppe;
+    }
+
+    public function getFach(){
+        return $this->fach;
+    }
+
+    public function setFach(string $fach){
+        return $this->fach = $fach;
+    }
+
+    public function getOrt(){
+        return $this->ort;
+    }
+
+    public function setOrt(string $ort){
+        return $this->ort = $ort;
+    }
 }
