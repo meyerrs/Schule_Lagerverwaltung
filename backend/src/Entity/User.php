93eda0262965c827b1410c02d176414d051ba7e0
Aeneas Kremer <aeneaskremer1@gmail.com>
2025-11-17 12:09:19 +0100

    users begin

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -1,6 +1,49 @@
 <?php
 
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'user')]
 class User
 {
+    #[ORM\Column(type: Types::INTEGER)]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    private int $id;
 
+    #[ORM\Column(type: Types::STRING)]
+    private string $firstname;
+
+    #[ORM\Column(type: Types::STRING)]
+    private string $lastname;
+
+    #[ORM\Column(type: Types::STRING)]
+    private string $password;
+
+    #[ORM\Column(type: Types::STRING)]
+    private string $role = "Lehrer";
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
 }
