8c25d68258d51765f319224d2262c80626bca2b5
Aeneas Kremer <aeneaskremer1@gmail.com>
2025-11-21 12:01:11 +0100

    better doctrine migrations configuration

---
+++ backend/migrations.php
@@ -0,0 +1,9 @@
+<?php
+// migrations.php
+return [
+    'migrations_paths' => [
+        // Namespace => Verzeichnis
+        'Migrations' => __DIR__ . '/data/Migrations',
+    ],
+    'all_or_nothing' => true,
+];
