a33f3dcfc9b0b642bc691183ea54af65da87dc4d
meyerrs <sb.meyer.yannis@gmail.com>
2026-02-06 11:17:43 +0100

    Inventory Design überarbeitet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import Inventory from "./pages/Inventory";
+
+
+
+
+// nur für Entwicklung
+const PREVIEW_INVENTORY = true;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {PREVIEW_INVENTORY ? (
+      <Inventory />
+    ) : (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )}
   </React.StrictMode>
 );
